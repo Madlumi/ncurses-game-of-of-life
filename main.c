b3638625699fa79f982c7010dc5177d814445fe6
Madanie <madi@localhost.localdomain>
2023-10-17 01:41:49 +0300

    game o life

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 #include <ncurses.h>
 #include <stdlib.h>
 
+#define SPD 100
+
 typedef struct _cell{
-int data;
+   int data;
 }cell;
 typedef struct _square{
    int x;
@@ -11,59 +13,77 @@ typedef struct _square{
    int w;
    int h;
 }square;
-void destroy_win(WINDOW *local_win);
-
-int main(int argc, char *argv[])
-{	WINDOW *win;
-	int startx, starty, width, height;
-	int ch;
-	initscr();			/* Start curses mode 		*/
-	cbreak();			/* Line buffering disabled, Pass on
-					 * everty thing to me 		*/
-	keypad(stdscr, TRUE);		/* I need that nifty F1 	*/
+void printArea(WINDOW *w, cell **c, square sq);
+void live(cell **area, cell **areaT,square sq);
+void init(){
+   initscr();
+   cbreak();
+   keypad(stdscr, TRUE);
+}
+int main(int argc, char *argv[]){
+   //Init
+   WINDOW *win;cell **area;cell **areaT;int startx, starty, width, height;int ch;
+   //ncuses init
+   init();
    square sq={.x=0,.y=0,.w=COLS-3,.h=LINES-2};
-   sq.x=(COLS - sq.w )/2;
-   sq.y=(LINES- sq.h )/2;
-	refresh();
+   sq.x=(COLS - sq.w )/2;sq.y=(LINES- sq.h )/2;
+   area= (cell **)calloc(sq.w, sizeof(cell *)); 
+   for(int i = 0;i < sq.w; ++i){area[i] = (cell *)calloc(sq.h, sizeof(cell));}
+
+   areaT= (cell **)calloc(sq.w, sizeof(cell *)); 
+   for(int i = 0;i < sq.w; ++i){areaT[i] = (cell *)calloc(sq.h, sizeof(cell));}
+   refresh();
    win = newwin(sq.h, sq.w, sq.y, sq.x);
-	box(win, 0 , 0);		
-	wrefresh(win);		
-   timeout(100);
+   box(win, 0 , 0);		
+   wrefresh(win);		
+   timeout(SPD);
+
+   //set area daaata
+   for (int yy = 1; yy < sq.h-1; yy++){
+      for (int xx = 1; xx < sq.w; xx++){
+         area[xx][yy].data=rand()%10<2;   
+   }}
    while(getch() != KEY_F(1)){
-      
-      for (int yy = 1; yy < sq.h-1; yy++){
-      int lw = rand()%sq.w-2;
-         for (int xx = 1; xx < lw; xx++){
-         mvwaddch(win,yy, xx, rand()%127);
-      }
-      }
-	box(win, 0 , 0);		
-	wrefresh(win);
+      printArea(win,area,sq);
+      box(win, 0 , 0);		
+      live(area,areaT,sq);
+      wrefresh(win);
    }
- 
-		
-	endwin();
-	return 0;
+   endwin();
+   return 0;
+}
+void live(cell **area, cell **areaT, square sq){
+   
+   for (int yy = 1; yy < sq.h-1; yy++){for (int xx = 1; xx < sq.w-1; xx++){
+      areaT[xx][yy].data=area[xx][yy].data;
+   }}
+
+   for (int yy = 1; yy < sq.h-1; yy++){for (int xx = 1; xx < sq.w-1; xx++){
+      int neigh=0;
+      for(int a = -1; a <=1 ; a++){
+         for(int b = -1; b <=1 ; b++){
+            if(a!=0 || b!=0){
+               neigh+=areaT[xx+a][yy+b].data;
+            }
+         }
+      }
+      if (areaT[xx][yy].data==1 && (neigh==2 || neigh==3)){
+         area[xx][yy].data=1;
+      }else if(neigh==3){
+         area[xx][yy].data=1;
+      }else{
+         area[xx][yy].data=0;
+      }
+      //areaT[xx][yy].data=area[xx][yy].data;
+   }}
 }
 
-void destroy_win(WINDOW *local_win)
-{	
-	/* box(local_win, ' ', ' '); : This won't produce the desired
-	 * result of erasing the window. It will leave it's four corners 
-	 * and so an ugly remnant of window. 
-	 */
-	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
-	/* The parameters taken are 
-	 * 1. win: the window on which to operate
-	 * 2. ls: character to be used for the left side of the window 
-	 * 3. rs: character to be used for the right side of the window 
-	 * 4. ts: character to be used for the top side of the window 
-	 * 5. bs: character to be used for the bottom side of the window 
-	 * 6. tl: character to be used for the top left corner of the window 
-	 * 7. tr: character to be used for the top right corner of the window 
-	 * 8. bl: character to be used for the bottom left corner of the window 
-	 * 9. br: character to be used for the bottom right corner of the window
-	 */
-	wrefresh(local_win);
-	delwin(local_win);
+void printArea(WINDOW *w, cell **area, square sq){
+   for (int yy = 1; yy < sq.h-1; yy++){for (int xx = 1; xx < sq.w-1; xx++){
+      if(area[xx][yy].data==0){
+      mvwaddch(w,yy, xx, ' ');
+      }else{
+      mvwaddch(w,yy, xx, '#');
+      }
+   }}
 }
